7bb5087202038b8b673cce42fb40832bbc64b05f
Andres Carranza <diacarry@gmail.com>
2019-10-22 18:01:13 -0500

    Create dictionary of sentences in English language

---
+++ resources/lang/en/sentences.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    'warningA' => 'NO INDIVIDUALS REGISTERED',
+
+    'tableEmail' => 'E-mail',
+    'tableFirstName' => 'First Name',
+    'tableLastname' => 'Lasta Name',
+
+    'individualTitleA' => 'Individuals',
+    'individualTitleB' => 'Individuals List ',
+    'individualButtonCreate' => 'Create new individual',
+];
