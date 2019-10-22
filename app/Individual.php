8569312e1ab647a04aea2ce9b1713534b6d71b6e
Andres Carranza <diacarry@gmail.com>
2019-10-22 16:39:30 -0500

    Configuration of the individual model

--- app/Individual.php
+++ app/Individual.php
@@ -6,5 +6,42 @@ use Illuminate\Database\Eloquent\Model;
 
 class Individual extends Model
 {
-    //
+    
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'individuals';
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'email';
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     * Options: true -> Auto-Increment; false -> No Auto-Increment
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+    /**
+     * The "type" of the auto-incrementing ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+    /**
+     * Indicates if the model should be timestamped.
+     * Options: true -> able; false -> enable
+     *
+     * @var bool
+     */
+    public $timestamps = true;
+    /**
+     * Names variables CREATED_AT and UPDATED_AT (default)
+     */
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
 }
